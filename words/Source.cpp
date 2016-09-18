8b95d727048dfb0d285885001f42c192a1e5fd3a
Samiul Hoque <samiul.hoque@enosisbd.com>
2016-09-18 09:43:00 +0600

    exe added

--- words/Source.cpp
+++ words/Source.cpp
@@ -1,23 +1,31 @@
 #include "word_manager.h"
 #include "console_helper.h"
 
+#include <iostream>
 #include <functional>
 
 using namespace std;
 
 int main()
 {
-	word_manager words{ "dict.xml" };
-	auto end = false;
-
-	while (!end)
+	try
 	{
-		run_command({
-			{"Insert",				bind(&word_manager::insert, &words)				},
-			{"Print",				bind(&word_manager::print, &words)				},
-			{"Quiz",				bind(&word_manager::quiz, &words)				},
-			{"Mark Learned Words",	bind(&word_manager::mark_learned_words, &words)	},
-			{"Exit",				[&end]() {end = true; }							}
-		}, false);
+		word_manager words{ "dict.xml" };
+		auto end = false;
+
+		while (!end)
+		{
+			run_command({
+				{"Insert",				bind(&word_manager::insert, &words)				},
+				{"Print",				bind(&word_manager::print, &words)				},
+				{"Quiz",				bind(&word_manager::quiz, &words)				},
+				{"Mark Learned Words",	bind(&word_manager::mark_learned_words, &words)	},
+				{"Exit",				[&end]() {end = true; }							}
+			}, false);
+		}
+	}
+	catch (exception &e)
+	{
+		cout << e.what() << ". Exiting.\n";
 	}
 }
